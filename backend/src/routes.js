f58cfd3d747e8b74221fb2158ec1dbe238772638
Julia Barbosa <59066234+jsilvabarb@users.noreply.github.com>
2021-11-28 20:52:37 -0300

    Editando nome das funções de FilmeController

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const FilmeController = require("./controllers/FilmeController");
 const PersonagemController = require("./controllers/PersonagemController");
-// const CenarioController = require("./controllers/CenarioController");
+const CenarioController = require("./controllers/CenarioController");
 
 const routes = express.Router();
 
-routes.get("/filmes", FilmeController.index);
-routes.post("/filmes", FilmeController.store);
+// Create e Post de filmes na tabela filmes
+routes.get("/filmes", FilmeController.GetAll);
+routes.post("/filmes", FilmeController.Add);
 
+// CRUD Personagens
 routes.get("/:id_filme/personagens", PersonagemController.GetAll);
 routes.post("/:id_filme/personagens", PersonagemController.Add);
 routes.put("/personagens/:id_personagem", PersonagemController.Update);
 routes.delete("/personagens/:id_personagem", PersonagemController.Delete);
+
+// Create, Post e Update, tabela cenarios
+routes.get("/:id_personagem/cenario", CenarioController.Get);
+routes.post("/addcenario/:id_personagem", CenarioController.Add);
+routes.put("/:id_personagem/cenario", CenarioController.Update);
 
 module.exports = routes;
