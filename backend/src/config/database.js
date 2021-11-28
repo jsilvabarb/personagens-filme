1362dc613de1d27a155fd34664f9325883c815fb
Julia Barbosa <59066234+jsilvabarb@users.noreply.github.com>
2021-11-28 09:24:45 -0300

    configurando conexão ao banco

---
+++ backend/src/config/database.js
@@ -0,0 +1,11 @@
+/* eslint-disable indent */
+module.exports = {
+    dialect: "mysql",
+    host: "localhost",
+    username: "root",
+    password: "root123",
+    database: "personagens_filme",  
+    define: {
+        underscored: true
+    }
+};
