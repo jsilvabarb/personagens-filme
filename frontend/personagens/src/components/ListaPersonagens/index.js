cbbabac40edf29e4945f6e315eb0f127e0a96a87
Julia Barbosa <59066234+jsilvabarb@users.noreply.github.com>
2021-11-28 20:57:18 -0300

    Editando componente ListaPersonagens

--- frontend/personagens/src/components/ListaPersonagens/index.js
+++ frontend/personagens/src/components/ListaPersonagens/index.js
@@ -1,34 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {FaTrashAlt}  from "react-icons/fa";
 import Personagensdb from "../../Personagensdb";
 import "./ListaPersonagens.css";
-// import ModalPersonagens from '../ModalPersonagens';
+import ModalDetalhesPersonagem from '../ModalDetalhesPersonagem';
 
 const ListaPersonagens = ({personagens}) => {
    
 
    const [modalVisivel, setModalVisivel] = useState(false);
+   const [cenario, setCenario] = useState();
 
-    const deletePersonagem = (e) => {
-        console.log(e.target.id);
-    //    Personagensdb.makeRequest("http://localhost:3001/personagens/" + e.target.id, "DELETE");
-    }
 
-    const id = personagens.id;   
+    const id = personagens.id; 
+
+    useEffect(() => {
+        Personagensdb.makeRequest("http://localhost:3001/"+id+"/cenario", "GET").then(function(response) {
+         response.json().then(function(Cenario) {
+            setCenario(Cenario);          
+         })
+        });    
+    }, [])
 
     return(
         <div className="personagens-area">
             <div className="personagens-lista">                          
                 <div id={personagens.id} className="personagens-lista-item">
                     
-                     <button className="btn-personagem-detalhe">
-                         <img src={personagens.imagem} alt={personagens.nome}></img>
-                         {personagens.id}
-                             <h5 id={personagens.id} className="delete" onClick={deletePersonagem}><FaTrashAlt/></h5>                           
-                    </button>                                            
-                </div>               
-            </div>
-          
+                     <button onClick={() => setModalVisivel(true)} className="btn-personagem-detalhe">
+                         <img src={personagens.imagem} alt={personagens.nome}></img>                               
+                    </button>
+                    
+                    {modalVisivel ? <ModalDetalhesPersonagem onClose={() => setModalVisivel(false)} idPersonagem={personagens.id} nomePersonagem={personagens.nome} descricaoPersonagem={personagens.descricao} imagemPersonagem={personagens.imagem} cenario={cenario}/> : null} 
+                                                                
+                </div>                                
+            </div>          
         </div>
     )  
 }
