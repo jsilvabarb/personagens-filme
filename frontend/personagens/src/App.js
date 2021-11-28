c6e8de86df6daf0f62c1606704f3ba03980ea0e1
Julia Barbosa <59066234+jsilvabarb@users.noreply.github.com>
2021-11-28 20:54:08 -0300

    Adicinado botão para adicionar filmes e alterando menu

--- frontend/personagens/src/App.js
+++ frontend/personagens/src/App.js
@@ -3,12 +3,14 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import Pernonagensdb from "./Personagensdb";
 import ListaFilmes from "./components/ListaFilmes";
+import ModalAddFilmes from "./components/ModalAddFilmes";
 
 
 
 export default () => {
   
   const [ listaFilmes, setListaFilmes ] = useState([{}]);
+  const [modalVisivel, setModalVisivel] = useState(false);
   useEffect(() => {
    Pernonagensdb.makeRequest("http://localhost:3001/filmes", "GET").then(function(response) {
     response.json().then(function(Filmes) {
@@ -19,8 +21,10 @@ export default () => {
   }, [])
   return (
     <div className="page">
-      <nav>
-        <h1>Animados</h1>
+      <nav className="menu">
+        <h1>ANIMADOS</h1>
+        <button onClick={() => setModalVisivel(true)}>+ Filmes</button>
+        {modalVisivel ? <ModalAddFilmes onClose={() => setModalVisivel(false)}/> : null}  
       </nav>
 
       <section className="filmes">
